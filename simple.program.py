061be172911abafd299eafd647d40c854009d9ff
prashant-00-22 <prashant220405@gmail.com>
2025-07-28 07:39:29 +0530

    Add files via upload
    #Task 1: Perform Basic Mathematical OperationsTask 1: Perform Basic Mathematical Operations

---
+++ simple.program.py
@@ -0,0 +1,18 @@
+#Task 1: Perform Basic Mathematical OperationsTask 1: Perform Basic Mathematical Operations
+
+x=float(input("enter a number"))
+y=float(input("enter a number"))
+Add=x+y
+Sub=x-y
+Mul=x*y
+Div=x/y
+print(Add,Sub,Mul,Div)
+
+
+#Task 2: Create a Personalized Greeting10
+
+a=input("first name")
+b=input("second name")
+print( a + b )
+print("how are you" , a+b ,"what are you doing?" )
+
